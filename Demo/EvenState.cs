4832a60ba36f97345d777d91a99b8de468063476
rlovely <rlovely@medallionis.com>
2024-01-08 11:35:27 -0500

    fixing indentation of demo classes

--- Demo/EvenState.cs
+++ Demo/EvenState.cs
@@ -1,13 +1,13 @@
 ﻿using RoadieRichStateMachine;
 
 class EvenState : State
+{
+	protected override void Enter(Dictionary<string, dynamic> vars) { }
+
+	protected override void Exit(Dictionary<string, dynamic> vars) { }
+
+	protected override void Inner(Dictionary<string, dynamic> vars)
 	{
-		protected override void Enter(Dictionary<string, dynamic> vars) { }
-
-		protected override void Exit(Dictionary<string, dynamic> vars) { }
-
-		protected override void Inner(Dictionary<string, dynamic> vars)
-		{
-			Console.WriteLine(" is even");
-		}
+		Console.WriteLine(" is even");
 	}
+}
